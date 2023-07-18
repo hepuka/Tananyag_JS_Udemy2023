58095f18d70c10c66fd3013751367384699f2473
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-07-18 17:03:18 +0200

    upd

--- Codewars/String/tömb.js
+++ Codewars/String/tömb.js
@@ -1,12 +1,22 @@
-var websites = new Array(10).fill("codewars");
-console.log(websites);
-
 //www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
 function stringToArray(string) {
   return string.split(" ");
 }
 console.log(stringToArray("Robin Singh"));
 
-var websites = [];
-websites = new Array(4).fill("codewars");
+
+var websites = new Array(4).fill("codewars");
 console.log(websites);
+
+function toCsvText(array) {
+  return array.join("\n");
+}
+
+console.log(
+    toCsvText([
+      [0, 1, 2, 3, 45],
+      [10, 11, 12, 13, 14],
+      [20, 21, 22, 23, 24],
+      [30, 31, 32, 33, 34],
+    ]),
+);
--- Codewars/codewars.js
+++ Codewars/codewars.js
@@ -1,12 +1,1 @@
-function toCsvText(array) {
-  return array.join("\n");
-}
 
-console.log(
-  toCsvText([
-    [0, 1, 2, 3, 45],
-    [10, 11, 12, 13, 14],
-    [20, 21, 22, 23, 24],
-    [30, 31, 32, 33, 34],
-  ]),
-);
