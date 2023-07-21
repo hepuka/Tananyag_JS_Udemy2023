ad842e531d1b5b305b232b6c39c835cbf5bd9515
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-07-21 17:37:00 +0200

    upd

---
+++ EPAM/Codewars tasks/Elementary_Expeditions-7kyu.js
@@ -0,0 +1,196 @@
+function differenceInAges(ages) {
+  const min = Math.min(...ages);
+  const max = Math.max(...ages);
+  return [min, max, max - min];
+}
+
+console.log(differenceInAges([82, 15, 6, 38, 35]));
+
+function descendingOrder(n) {
+  return Number(
+    String(n)
+      .split("")
+      .sort((a, b) => b - a)
+      .join(""),
+  );
+}
+
+console.log(descendingOrder(1021));
+
+function arrayMadness(a, b) {
+  return (
+    a.reduce((acc, curr) => acc + curr ** 2, 0) >
+    b.reduce((acc, curr) => acc + curr ** 3, 0)
+  );
+}
+
+console.log(arrayMadness([5, 3, 2, 4, 1], [15]));
+
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  return birds.filter((item) => !geese.includes(item));
+}
+
+console.log(
+  gooseFilter([
+    "Mallard",
+    "Hook Bill",
+    "African",
+    "Crested",
+    "Pilgrim",
+    "Toulouse",
+    "Blue Swedish",
+  ]),
+);
+
+function firstNonConsecutive(arr) {
+  const num = arr.find((item, index) => item !== index + arr[0]);
+  return Number.isInteger(num) ? num : null;
+}
+
+console.log(firstNonConsecutive([1, 2, 3, 4, 5]));
+
+function getSum(a, b) {
+  let sum = 0;
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+  if (a === b) return a;
+
+  for (let i = min; i <= max; i++) sum += i;
+
+  return sum;
+}
+
+console.log(getSum(5, -1));
+
+var Ball = function (ballType) {
+  this.ballType = ballType || "regular";
+};
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+String.prototype.isUpperCase = function () {
+  // return this == this.toUpperCase();
+  return !/[a-z]/.test(this);
+};
+
+// This Cube class needs help
+// Implement the constructor so that it can take an integer for the side or no args
+class Cube {
+  constructor(side = 0) {
+    this.side = Math.abs(side);
+  }
+
+  getSide() {
+    return this.side;
+  }
+
+  setSide(n) {
+    this.side = Math.abs(n);
+  }
+}
+
+var Person = function () {
+  var person = {
+    _name: "Leroy",
+    _friends: [],
+    fillFriends(f) {
+      this._friends.push(...f);
+    },
+  };
+  return person;
+};
+
+function htmlspecialchars(formData) {
+  let chars = {
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "&": "&amp;",
+  };
+
+  // return formData
+  //   .split("")
+  //   .map((item) => chars[item] || item)
+  //   .join("");
+
+  return formData.replace(/[<>"&]/g, (item) => chars[item]);
+}
+
+console.log(htmlspecialchars("<h2>Hello World</h2>"));
+
+function enough(cap, on, wait) {
+  return on + wait < cap ? 0 : Math.abs(wait - cap + on);
+}
+
+console.log(enough(54, 10, 77));
+
+function highAndLow(numbers) {
+  const nums = numbers.split(" ").map(Number);
+
+  return [Math.max(...nums), Math.min(...nums)].join(" ");
+}
+
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+
+function getCount(str) {
+  return str.split("").filter((item) => "aeiouAEIOU".includes(item)).length;
+  // return str.replace(/[^aeiou]/gi, '').length;
+}
+
+console.log(getCount("abracadabra"));
+
+function solution(nums) {
+  return nums === null ? [] : nums.sort((a, b) => a - b);
+}
+
+console.log(solution([]));
+
+function accum(s) {
+  return s
+    .toLowerCase()
+    .split("")
+    .map((item, index) => item.toUpperCase() + item.repeat(index))
+    .join("-");
+}
+
+console.log(accum("abcD"));
+
+function isIsogram(str) {
+  return str.length === new Set(str.toLowerCase()).size;
+}
+
+console.log(isIsogram("moOse"));
+
+// return masked string
+function maskify(cc) {
+  return cc.slice(-4).padStart(cc.length, "#");
+}
+
+console.log(maskify("4556364607935616"));
+
+function DNAStrand(dna) {
+  let letters = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C",
+  };
+  return dna.replace(/[ATCG]/g, (item) => letters[item]);
+  // return dna.replace(/./g, (item) => letters[item]);
+}
+
+console.log(DNAStrand("GTAT"));
+
+var findDigit = function (num, nth) {
+  if (nth <= 0) return -1;
+
+  let nums = "" + Math.abs(num);
+
+  return nums.slice(nums.length - nth, nums.length - nth + 1) * 1;
+};
+
+console.log(findDigit(5673, 7));
--- EPAM/Codewars tasks/Jealous_Jumpers-8kyu.js
+++ EPAM/Codewars tasks/Jealous_Jumpers-8kyu.js
@@ -64,3 +64,95 @@ function hexToDec(hexString) {
 }
 
 console.log(hexToDec("FF"));
+
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+
+console.log(repeatStr(3, "*"));
+
+function maps(x) {
+  return x.map((item) => item * 2);
+}
+
+console.log(maps([1, 2, 3]));
+
+// add the value "codewars" to the websites array 1,000 times
+var websites = [];
+
+for (let i = 1; i <= 1000; i++) {
+  websites.push("codewars");
+}
+
+function greet(name) {
+  return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
+
+console.log(greet("Johnny"));
+
+var array1 = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+
+function countSheeps(arrayOfSheep) {
+  // return arrayOfSheep.reduce((acc, curr) =>
+  //   curr === true ? acc + curr : acc + 0,
+  // );
+  return arrayOfSheep.filter(Boolean).length;
+}
+
+console.log(countSheeps(array1));
+
+function solution(a, b) {
+  return a.length > b.length ? b + a + b : a + b + a;
+}
+
+console.log(solution("U", "False"));
+
+function addLength(str) {
+  return str.split(" ").map((item) => `${item} ${item.length}`);
+}
+
+console.log(addLength("apple ban"));
+
+//tömb feltöltése Array() and fill()
+const array = Array(10)
+  .fill(0)
+  .map((item, index) => index);
+
+console.log(array);
+
+function divisibleBy(numbers, divisor) {
+  return numbers.filter((item) => item % divisor === 0);
+}
+
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2));
+
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" ");
+}
+
+console.log(reverseWords("hello world!"));
