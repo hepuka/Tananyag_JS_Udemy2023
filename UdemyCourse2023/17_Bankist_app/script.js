567f3ecffe86e1ad1007927c69f84173220841a3
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-10-02 16:59:27 +0200

    upd

--- UdemyCourse2023/17_Bankist_app/script.js
+++ UdemyCourse2023/17_Bankist_app/script.js
@@ -1,77 +1,77 @@
-'use strict';
+"use strict";
 
 // BANKIST APP
-
+//user kz, jelszó 1111
 // Data
 
 // DIFFERENT DATA! Contains movement dates, currency and locale
 
 const account1 = {
-  owner: 'Kun-Fagyal Zoltán',
+  owner: "Kun-Fagyal Zoltán",
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-07-26T17:01:17.194Z',
-    '2020-07-28T23:36:17.929Z',
-    '2020-08-01T10:51:36.790Z',
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-07-26T17:01:17.194Z",
+    "2020-07-28T23:36:17.929Z",
+    "2020-08-01T10:51:36.790Z",
   ],
-  currency: 'EUR',
+  currency: "EUR",
 };
 
 const account2 = {
-  owner: 'Kun-Fagyal Katalin',
+  owner: "Kun-Fagyal Katalin",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
   ],
-  currency: 'USD',
+  currency: "USD",
 };
 
 const accounts = [account1, account2];
 
 /////////////////////////////////////////////////
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+const labelWelcome = document.querySelector(".welcome");
+const labelDate = document.querySelector(".date");
+const labelBalance = document.querySelector(".balance__value");
+const labelSumIn = document.querySelector(".summary__value--in");
+const labelSumOut = document.querySelector(".summary__value--out");
+const labelSumInterest = document.querySelector(".summary__value--interest");
+const labelTimer = document.querySelector(".timer");
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+const containerApp = document.querySelector(".app");
+const containerMovements = document.querySelector(".movements");
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+const btnLogin = document.querySelector(".login__btn");
+const btnTransfer = document.querySelector(".form__btn--transfer");
+const btnLoan = document.querySelector(".form__btn--loan");
+const btnClose = document.querySelector(".form__btn--close");
+const btnSort = document.querySelector(".btn--sort");
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginPin = document.querySelector(".login__input--pin");
+const inputTransferTo = document.querySelector(".form__input--to");
+const inputTransferAmount = document.querySelector(".form__input--amount");
+const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const inputCloseUsername = document.querySelector(".form__input--user");
+const inputClosePin = document.querySelector(".form__input--pin");
 
 /////////////////////////////////////////////////
 ////////////////////////////////////////////////
@@ -85,8 +85,8 @@ const formatMovementDate = function (date, locale) {
   const daysPassed = calcDaysPassed(new Date(), date);
   console.log(daysPassed);
 
-  if (daysPassed === 0) return 'Today';
-  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed === 0) return "Today";
+  if (daysPassed === 1) return "Yesterday";
   if (daysPassed <= 7) return `${daysPassed} days ago`;
 
   // const day = `${date.getDate()}`.padStart(2, 0);
@@ -98,7 +98,7 @@ const formatMovementDate = function (date, locale) {
 
 const formatCur = function (value, locale, currency) {
   return new Intl.NumberFormat(locale, {
-    style: 'currency',
+    style: "currency",
     currency: currency,
   }).format(value);
 };
@@ -129,9 +129,9 @@ const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
       .toLowerCase()
-      .split(' ') //szétszedjük a nevet szavakra és azt egy tömben elemenként visszakapjuk
-      .map(name => name[0]) //bejárjuk ezt a visszakott tömböt és az egyes elemek kezdőbetűjéből kapunk egy újabb tömböt
-      .join(''); //ennek a tömbnek az elemeit rakjuk egy string-é, és kapjuk meg a username-et
+      .split(" ") //szétszedjük a nevet szavakra és azt egy tömben elemenként visszakapjuk
+      .map((name) => name[0]) //bejárjuk ezt a visszakott tömböt és az egyes elemek kezdőbetűjéből kapunk egy újabb tömböt
+      .join(""); //ennek a tömbnek az elemeit rakjuk egy string-é, és kapjuk meg a username-et
   });
 
   //nem kell return mert az egyes objektumokat módosítottuk közvetlenül ezzel a metódussal
@@ -140,7 +140,7 @@ createUsernames(accounts);
 
 //2.táblázatba rakja az adatokat
 const displayMovements = function (acc, sort = false) {
-  containerMovements.innerHTML = '';
+  containerMovements.innerHTML = "";
 
   //elsőnek a slice-al készítek egy másolatot a tömbről majd arra hívom meg a sort metódust
   //azért kell a másolat mert ellenkező esetben az eredeti tömböt akarom visszakapni
@@ -149,7 +149,7 @@ const displayMovements = function (acc, sort = false) {
     : acc.movements;
 
   movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const type = mov > 0 ? "deposit" : "withdrawal";
 
     const date = new Date(acc.movementsDates[i]);
     const displayDate = formatMovementDate(date, acc.locale);
@@ -166,7 +166,7 @@ const displayMovements = function (acc, sort = false) {
       </div>
     `;
 
-    containerMovements.insertAdjacentHTML('afterbegin', html);
+    containerMovements.insertAdjacentHTML("afterbegin", html);
   });
 };
 
@@ -179,18 +179,18 @@ const calcDisplayBalance = function (acc) {
 //4.az alkalmazás alján kiírja az IN, OUT, INTEREST értékeket
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
-    .filter(mov => mov > 0)
+    .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
-    .filter(mov => mov < 0)
+    .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
 
   const interest = acc.movements
-    .filter(mov => mov > 0)
-    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * acc.interestRate) / 100)
 
     //csak akkor van kamat ha a kamat legalább 1 euró, ezért a map által visszaadott tömbre megint egy filtert kellett rakni
     .filter((int, i, arr) => {
@@ -217,7 +217,7 @@ const startLogOutTimer = function () {
     if (time === 0) {
       //megszakítom a timer futását
       clearInterval(timer);
-      labelWelcome.textContent = 'Log in to get started';
+      labelWelcome.textContent = "Log in to get started";
       containerApp.style.opacity = 0;
     }
 
@@ -257,7 +257,7 @@ let currentAccount, timer;
 // containerApp.style.opacity = 100;
 
 //bejelentkező gombra való tevékenykedés
-btnLogin.addEventListener('click', function (e) {
+btnLogin.addEventListener("click", function (e) {
   // Prevent form from submitting
 
   //megállítja az oldal frissülését
@@ -265,26 +265,26 @@ btnLogin.addEventListener('click', function (e) {
 
   //kimentjük a változóba a username mezőben megadott nevet, hogy azt ezután összehasonlítsuk a PIN-ben megadott kóddal
   currentAccount = accounts.find(
-    acc => acc.username === inputLoginUsername.value
+    (acc) => acc.username === inputLoginUsername.value
   );
 
   //a PIN mezőben megadott értéket számmá kell konvertálni, minden megadott érték string-ként kerül megadásra az oldalon
   if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(
-      ' '
+      " "
     )}`;
     containerApp.style.opacity = 100;
 
     // Create current date and time
     const now = new Date();
     const options = {
-      hour: 'numeric',
-      minute: 'numeric',
-      day: 'numeric',
-      month: 'long', //'number' azámként írja ki a hónapot
-      year: 'numeric',
-      weekday: 'long',
+      hour: "numeric",
+      minute: "numeric",
+      day: "numeric",
+      month: "long", //'number' azámként írja ki a hónapot
+      year: "numeric",
+      weekday: "long",
     };
 
     //a böngészőben beállított nyelvet kérdezi le és ezt adom meg mint paraméter az Intl.DateTimeFormat - nak
@@ -306,7 +306,7 @@ btnLogin.addEventListener('click', function (e) {
     // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
     // Clear input fields
-    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginUsername.value = inputLoginPin.value = "";
     inputLoginPin.blur();
 
     // Timer
@@ -320,7 +320,7 @@ btnLogin.addEventListener('click', function (e) {
 });
 
 //pénzutalás egyik felhasználótól a másiknak
-btnTransfer.addEventListener('click', function (e) {
+btnTransfer.addEventListener("click", function (e) {
   e.preventDefault();
 
   //érték tárolása a transfer to mezőből
@@ -328,11 +328,11 @@ btnTransfer.addEventListener('click', function (e) {
 
   //receiverAcc mezőben megadott felhasználó mentése
   const receiverAcc = accounts.find(
-    acc => acc.username === inputTransferTo.value
+    (acc) => acc.username === inputTransferTo.value
   );
 
   //mező törlése
-  inputTransferAmount.value = inputTransferTo.value = '';
+  inputTransferAmount.value = inputTransferTo.value = "";
 
   //ellenőrzés az utalás előtt
   if (
@@ -361,14 +361,17 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
-btnLoan.addEventListener('click', function (e) {
+btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
 
   //amount mező értékét az amount változóba mentjük
   const amount = Math.floor(inputLoanAmount.value);
 
   //ha a megadott mennyiség pozitív és a movements tömbben van olyan elem ami nagyobb mint az igényelt összeg 10%-a
-  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
     //a kölcsön folyósítása 2,5 mp múlva lesz
     setTimeout(function () {
       // Add movement
@@ -387,10 +390,10 @@ btnLoan.addEventListener('click', function (e) {
     }, 2500);
   }
 
-  inputLoanAmount.value = '';
+  inputLoanAmount.value = "";
 });
 
-btnClose.addEventListener('click', function (e) {
+btnClose.addEventListener("click", function (e) {
   e.preventDefault();
 
   //ellenőrizzük, hogy a megadott username és a pin létezik-e
@@ -399,7 +402,7 @@ btnClose.addEventListener('click', function (e) {
     +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
-      acc => acc.username === currentAccount.username
+      (acc) => acc.username === currentAccount.username
     );
     console.log(index);
     // .indexOf(23)
@@ -411,12 +414,12 @@ btnClose.addEventListener('click', function (e) {
     containerApp.style.opacity = 0;
   }
 
-  inputCloseUsername.value = inputClosePin.value = '';
+  inputCloseUsername.value = inputClosePin.value = "";
 });
 
 //kilistázott utalások rendezése
 let sorted = false;
-btnSort.addEventListener('click', function (e) {
+btnSort.addEventListener("click", function (e) {
   e.preventDefault();
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
