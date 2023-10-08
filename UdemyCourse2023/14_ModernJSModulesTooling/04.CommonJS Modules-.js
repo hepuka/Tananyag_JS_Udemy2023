4c4b5482d268501d726134726d3f7a4eacf48359
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-10-08 12:32:55 +0200

    upd

--- UdemyCourse2023/14_ModernJSModulesTooling/04.CommonJS Modules-.js
+++ UdemyCourse2023/14_ModernJSModulesTooling/04.CommonJS Modules-.js
@@ -1,13 +1,12 @@
 //NodeJS rendszerben való exportálás és importálás szintaxisa
 
-
 //Export
-export.addTocart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(
-      `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
-    );
-  };
-  
-  //Import
-  const { addTocart } = require('./shoppingCart.js');
+// export.addTocart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(
+//       `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+//     );
+//   };
+
+//   //Import
+//   const { addTocart } = require('./shoppingCart.js');
