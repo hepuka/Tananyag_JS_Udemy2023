5f8143b15f80f5d69433e35904a6be353f715ed1
Kun-Fagyal Zoltán <kunzoltan7707@gmail.com>
2023-10-19 21:39:02 +0200

    upd

--- EPAM/Codewars tasks/Various_Visuals.js
+++ EPAM/Codewars tasks/Various_Visuals.js
@@ -17,3 +17,45 @@ function diamond(n) {
 }
 
 console.log(diamond(5));
+
+// function chained(functions) {
+//   return function inner(a) {
+//     for (var b = 0; b < functions.length; b++) {
+//       a = functions[b](a);
+//     }
+//     return a;
+//   };
+// }
+
+function f1(x) {
+  return x * 2;
+}
+function f2(x) {
+  return x + 2;
+}
+function f3(x) {
+  return Math.pow(x, 2);
+}
+
+// console.log(chained(f1, f2, f3)(4));
+
+function bestMatch(ALAHLYGoals, zamalekGoals) {
+  const goals = ALAHLYGoals.map((_, index) =>
+    Math.abs(ALAHLYGoals[index] - zamalekGoals[index])
+  );
+
+  const filteredArray = goals.map((item, index) =>
+    item === Math.min(...goals)
+      ? Math.max(ALAHLYGoals[index], zamalekGoals[index])
+      : 0
+  );
+
+  return filteredArray.indexOf(Math.max(...filteredArray));
+}
+
+console.log(
+  bestMatch(
+    [5, 12, 7, 20, 15, 9, 12, 10, 12, 13, 7, 16, 11, 12, 14],
+    [1, 6, 3, 10, 10, 0, 10, 2, 5, 7, 4, 9, 5, 5, 6]
+  )
+);
